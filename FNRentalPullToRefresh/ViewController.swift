2a7f6391c9265a4ff0616a8b21d38adffc5a97ac
LuWeijun <luweijun@weidian.com>
2016-05-20 23:06:43 +0800

    Initial Commit

---
+++ FNRentalPullToRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FNRentalPullToRefresh
+//
+//  Created by Fnoz on 16/5/20.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
